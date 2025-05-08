58a5189be2f657917fd9dd079e356c0c677352dd
huangyizhitt <huangyizhi@hnu.edu.cn>
2025-05-08 19:29:50 +0800

    [why] 1. Added synchronization when creating tasks       2. Added user interface to the simulator
    [who] huangyizhi

--- OS_evolution/Task Schedule/src/scheduler.c
+++ OS_evolution/Task Schedule/src/scheduler.c
@@ -4,22 +4,17 @@
 #include <stdio.h>
 #include <signal.h>
 
-#define MAX_NUM_CPU     256
-
 #define SCHEDULER_PRIORITY 99
+#define TASK_PRIORITY   50
 
 static char scheduler_stop = 0;
 
 extern struct sched_class table_sched_class;
 
-static struct scheduler *schedulers[MAX_NUM_CPU] = {NULL};
+//static struct scheduler *schedulers[MAX_NUM_CPU] = {NULL};
+//static struct task_struct *tasks_persched[MAX_NUM_CPU] = {NULL};
+//static int tasks_num_percpu[MAX_NUM_CPU] = {0};
 struct sched_class *sched_class_arr[NUM_SCHED_STRATEGY] = {&table_sched_class, NULL, NULL};
-
-static inline void regist_scheduler(struct scheduler *scheduler, int cpu, enum schedule_strategy_t sched_strategy)
-{
-    schedulers[cpu] = scheduler;
-    schedulers[cpu]->sched_class = sched_class_arr[sched_strategy];
-}
 
 static void kill_tasks(struct scheduler* sched)
 {
@@ -46,41 +41,75 @@ ret:
     return NULL;
 }
 
-int init_scheduler(struct scheduler *sched, struct sched_class *sched_class, int cpu, struct task_struct *tasks, int task_num)
+static void inline wait_tasks_init(struct scheduler *sched)
+{
+    pthread_mutex_lock(&sched->mutex);
+    while (sched->arrived_task_num < sched->task_num) {
+        pthread_cond_wait(&sched->cond, &sched->mutex);
+    }
+    pthread_mutex_unlock(&sched->mutex);
+}
+
+void init_scheduler(struct scheduler *sched, struct sched_class *sched_class, int cpu, struct task_struct *tasks, int task_num)
 {
     // 初始化成员变量
     INIT_LIST_HEAD(&sched->ready_queue);
     sched->tasks = tasks;
     sched->task_num = task_num;
+    sched->arrived_task_num = 0;
     sched->cpu = cpu;
     sched->current_task = NULL;
     sched->sched_class = sched_class;
+    pthread_mutex_init(&sched->mutex, NULL);
+    pthread_cond_init(&sched->cond, NULL);
 
-    for(int i = 0; i < task_num; i++) {
-        tasks[i].sched = sched;
+    for(int i = 0; i < sched->task_num; i++) {
+        sched->tasks[i].sched = sched;
     }
+}
 
+int create_scheduler_threads(struct scheduler *sched)
+{
     // 创建调度器线程 TODO 设置pthread的亲、RT和优先级属性
-    int ret = pthread_create(&sched->tid, NULL, scheduler_tick, (void *)sched);
+    pthread_attr_t attr;
+    struct sched_param scheduler_param;
+
+    pthread_attr_init(&attr);
+    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+
+    scheduler_param.sched_priority = SCHEDULER_PRIORITY;  // 高于普通线程
+    pthread_attr_setschedparam(&attr, &scheduler_param);
+
+    pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+
+    int ret = pthread_create(&sched->tid, &attr, scheduler_tick, (void *)sched);
     if(ret != 0) {
         printf("Create scheduler thread fail!\n");
         return RET_FAIL;
     }
 
-    struct sched_param scheduler_tick_param;
-    scheduler_tick_param.sched_priority = SCHEDULER_PRIORITY;
-    pthread_setschedparam(sched->tid, SCHED_FIFO, &scheduler_tick_param);
+    create_tasks(sched->tasks, sched->task_num);
+    wait_tasks_init(sched);
 
     return RET_SUCCESS;
 }
 
-void deinit_scheduler(struct scheduler *sched)
+void join_scheduler_threads(struct scheduler *sched)
 {
     pthread_join(sched->tid, NULL);
+
+    destroy_tasks(sched->tasks, sched->task_num);
 }
 
-int create_scheduler(int cpu, enum schedule_strategy_t sched_strategy, struct task_struct *tasks, int task_num)
+void deinit_scheduler(struct scheduler *sched)
 {
+    pthread_mutex_destroy(&sched->mutex);
+    pthread_cond_destroy(&sched->cond);
+}
+
+struct scheduler *create_scheduler(int cpu, enum schedule_strategy_t sched_strategy, struct task_struct *tasks, int task_num)
+{
+/*  已由调用者保证  
     if(cpu >= MAX_NUM_CPU || cpu < 0) {
         printf("The cpu is not within the managed range\n");
         return RET_FAIL;
@@ -89,44 +118,51 @@ int create_scheduler(int cpu, enum schedule_strategy_t sched_strategy, struct ta
     if(sched_strategy >= NUM_SCHED_STRATEGY || sched_strategy < TABLE) {
         printf("Can not support the %d strategy\n", sched_strategy);
         return RET_FAIL;
-    }
+    } */
 
+    struct scheduler *sched = NULL;
     struct sched_class *sched_class = sched_class_arr[sched_strategy];
     if(sched_class) {
-        schedulers[cpu] = sched_class->scheduler_create(sched_class, cpu, tasks, task_num);
-        if(!schedulers[cpu]) {
+        sched = sched_class->scheduler_create(sched_class, cpu, tasks, task_num);
+        if(!sched) {
             printf("Create schedulers fail!\n");
-            return RET_FAIL;
+            return NULL;
         }
     } else {
         printf("Can not support the %d strategy\n", sched_strategy);
-        return RET_FAIL;
+        return NULL;
     }
 
-    return RET_SUCCESS;
+    return sched;
 }
 
-void destroy_scheduler(int cpu)
+void destroy_scheduler(struct scheduler *scheduler)
 {
-    if(schedulers[cpu]) {
-        struct sched_class *sched_class = schedulers[cpu]->sched_class;
-        sched_class->scheduler_destroy(schedulers[cpu]);
-    }
+    // 由调用者保证scheduler的合法性
+    struct sched_class *sched_class = scheduler->sched_class;
+    sched_class->scheduler_destroy(scheduler);
 }
 
-void run_scheduler()
+void run_scheduler(struct scheduler *scheduler)
 {
-    for(int cpu = 0; cpu < MAX_NUM_CPU; cpu++) {
-        if(schedulers[cpu]) {
-            struct sched_class *sched_class = schedulers[cpu]->sched_class;
-            sched_class->scheduler_start(schedulers[cpu]);
-        }
-    }
+    // 由调用者保证scheduler的合法性
+    struct sched_class *sched_class = scheduler->sched_class;
+    sched_class->scheduler_start(scheduler);
 }
 
 static void task_wait_resume(struct task_struct *task)
 {
     pthread_mutex_lock(&task->mutex);
+
+    if(task->task_state == INIT) {
+        pthread_mutex_lock(&task->sched->mutex);
+        task->sched->arrived_task_num++;
+        task->task_state = SUSPENDED;
+        if(task->sched->arrived_task_num == task->sched->task_num) {
+            pthread_cond_signal(&task->sched->cond);
+        }
+        pthread_mutex_unlock(&task->sched->mutex);
+    }
         
     // 如果任务当前没有运行，则等待调度器唤醒
     while(task->task_state != RESUMING) {
@@ -175,94 +211,84 @@ static void *single_shot_task(void *args)
     return NULL;
 }
 
-struct task_struct *create_task_struct()
+void load_tasks(struct task_struct_info *task_info_arr, int num, struct task_struct *tasks_persched[], int *tasks_num_percpu)
 {
-    struct task_struct *task = (struct task_struct *)malloc(sizeof(struct task_struct));
-    // TODO 未来加入读取arxml创建任务
-    return task;
-}
+    if(!task_info_arr || num < 0) {
+        printf("Load tasks info parameter error\n");
+        exit(-1);
+    }
 
-struct task_struct *create_task(struct task_struct_info *info, task_function func)
-{
-    struct task_struct *task = (struct task_struct *)malloc(sizeof(struct task_struct));
-    if(task) {
-        task->priority = info->priority;
-        task->cpu = info->cpu;
-        task->period = info->period;
-        task->is_preempted = 0;
-        task->table_id = info->table_id;
-        task->task_type = info->task_type;
-        task->task_state = SUSPENDED;
-        task->pfunc = func;
-
-        pthread_mutex_init(&task->mutex, NULL);
-        pthread_cond_init(&task->cond_var, NULL);
-
-        switch(task->task_type) {
-            case BASIC_PERIODIC:
-                pthread_create(&task->tid, NULL, periodic_task, (void *)task);
-                break;
-            
-            case BASIC_SINGLE_SHOT:
-                pthread_create(&task->tid, NULL, single_shot_task, (void *)task);
-                break;
-
-            case EXTENDED_PERIODIC:
-                // TODO
-                break;
-
-            case EXTENDED_SINGLE_SHOT:
-
-                break;
-
-            case ISR:
-
-                break;
-
-            default:
-
-                break;
+    // 统计各CPU上tasks的数量，并创建对应的task结构
+    for(int i = 0; i < num; i++) {
+        int cpu = task_info_arr->cpu;
+        if(cpu < MAX_NUM_CPU) {
+            tasks_num_percpu[cpu]++;
+        } else {
+            printf("CPU %d is not online, task %p is abandoned\n", cpu, task_info_arr);
         }
     }
 
-    return task;
-}
+    for(int i = 0; i < MAX_NUM_CPU; i++) {
+        if(tasks_num_percpu[i] > 0) {
+            tasks_persched[i] = (struct task_struct *)malloc(sizeof(struct task_struct) * tasks_num_percpu[i]);
+        }
+    }
 
-void destroy_task(struct task_struct *task)
-{
-    if(task) {
-        pthread_join(task->tid, NULL);
-        pthread_mutex_destroy(&task->mutex);
-        pthread_cond_destroy(&task->cond_var);
-        free(task);
-    }      
-}
+    // 载入task信息到task结构，并初始化task结构
+    int tasks_index_percpu[MAX_NUM_CPU] = {0};
+    
+    for(int i = 0; i < num; i++) {
+        int cpu = task_info_arr->cpu;
+        int index = tasks_index_percpu[cpu]++;
 
-void create_tasks(struct task_struct *tasks, struct task_struct_info *task_struct_infos, int num_tasks)
-{
-    for(int i = 0; i < num_tasks; i++) {
-        struct task_struct *task = &tasks[i];
-        struct task_struct_info *info = &task_struct_infos[i];
+        if(index >= tasks_num_percpu[cpu]) {
+            printf("Load task info error, index %d out of bounds %d\n", index, tasks_num_percpu[cpu]);
+            exit(-1);
+        }
+
+        struct task_struct *task = &tasks_persched[cpu][index];
+        struct task_struct_info *info = &task_info_arr[i];
+
         task->priority = info->priority;
         task->cpu = info->cpu;
         task->period = info->period;
         task->is_preempted = 0;
         task->table_id = info->table_id;
         task->task_type = info->task_type;
-        task->task_state = SUSPENDED;
+        task->task_state = INIT;
         task->pfunc = info->pfunc;
 
         pthread_mutex_init(&task->mutex, NULL);
         pthread_cond_init(&task->cond_var, NULL);
-        printf("task[%d]:%p, period: %d, priority: %d, type: %d\n", i, task, task->period, task->priority, task->task_type);
+
+        printf("task %p on CPU %d, period: %d, priority: %d, type: %d\n", task, cpu, task->period, task->priority, task->task_type);
+    }
+
+}
+
+void create_tasks(struct task_struct *tasks, int num_tasks)
+{
+    pthread_attr_t attr;
+    struct sched_param task_param;
+
+    pthread_attr_init(&attr);
+    pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
+
+    task_param.sched_priority = TASK_PRIORITY;  // 高于普通线程
+    pthread_attr_setschedparam(&attr, &task_param);
+
+    pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
+
+    for(int i = 0; i < num_tasks; i++) {
+        struct task_struct *task = &tasks[i];
 
         switch(task->task_type) {
             case BASIC_PERIODIC:
-                pthread_create(&task->tid, NULL, periodic_task, (void *)task);
+                pthread_create(&task->tid, &attr, periodic_task, (void *)task);
                 break;
             
             case BASIC_SINGLE_SHOT:
-                pthread_create(&task->tid, NULL, single_shot_task, (void *)task);
+                pthread_create(&task->tid, &attr, single_shot_task, (void *)task);
                 break;
 
             case EXTENDED_PERIODIC:
--- OS_evolution/Task Schedule/unit_test/table_scheduler_create_test.c
+++ OS_evolution/Task Schedule/unit_test/table_scheduler_create_test.c
@@ -1,10 +1,11 @@
-#include "scheduler.h"
+#include "simulator.h"
 #include <stdlib.h>
 #include <stdio.h>
 
-int period_arr[] = {1,10,50,100,1000};
+// int period_arr[] = {10,50,100,500,1000,10000};
+int period_arr[] = {1,5,10,100,1000};
 int period_arr_len = sizeof(period_arr)/sizeof(int);
-int cpu_arr[] = {0};
+int cpu_arr[] = {8};
 int cpu_arr_len = sizeof(cpu_arr)/sizeof(int);
 
 static void test_task()
@@ -14,10 +15,9 @@ static void test_task()
 
 // table_scheduler暂时要求table_id是连续的
 
-static struct task_struct *user_create_tasks_random(int cpu, int num_task)
+static struct task_struct_info *user_create_tasksinfo_random(int cpu, int num_task)
 {
-    struct task_struct *tasks = (struct task_struct *)malloc(sizeof(struct task_struct)*num_task);
-    struct task_struct_info task_infos[num_task];
+    struct task_struct_info *task_infos = (struct task_struct_info *)malloc(sizeof(struct task_struct_info)*num_task);
 
     int table_id = 0, max_table_id = num_task / 3;
 
@@ -31,41 +31,25 @@ static struct task_struct *user_create_tasks_random(int cpu, int num_task)
         task_infos[i].pfunc = test_task;
     }
 
-    create_tasks(tasks, task_infos, num_task);
-
-    return tasks;
+    return task_infos;
 }
 
-static void user_destroy_tasks(struct task_struct *tasks, int num_task)
+static void user_destroy_tasksinfo(struct task_struct_info *tasks_info)
 {
-    if(tasks) {
-        destroy_tasks(tasks, num_task);
-        free(tasks);
+    if(tasks_info) {
+        free(tasks_info);
     }
 }
 
 int main(int argc, char **argv)
 {
-    struct task_struct *tasks_percpu[cpu_arr_len];
-    for(int i = 0; i < cpu_arr_len; i++) {
-        tasks_percpu[i] = user_create_tasks_random(cpu_arr[i], 10);
-    }
+    struct task_struct_info *info_arr = user_create_tasksinfo_random(0, 10);
 
-    sleep(10);
+    create_simulator(info_arr, 10, TABLE);
 
-    for(int i = 0; i < cpu_arr_len; i++) {
-        create_scheduler(cpu_arr[i], TABLE, tasks_percpu[i], 10);
-    }
-
-    run_scheduler();
-
-    for(int i = 0; i < cpu_arr_len; i++) {
-        destroy_scheduler(cpu_arr[i]);
-    }
-
-    for(int i = 0; i < cpu_arr_len; i++) {
-        user_destroy_tasks(tasks_percpu[i], 10);
-    }
+    run_simulator();
+    
+    destroy_simulator();
 
     return 0;
 }
